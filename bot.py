0282f99118637f604ea54c1d8826f98dc9c5106b
Oleksii Chepur <60858989+chepuralex1991@users.noreply.github.com>
2023-03-31 09:20:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 ﻿import telebot
 import requests
-api_key = "Y622QFPz2idteWegBpn3kRUoUXay7Dgk"
-bot_token = "5401760387:AAE-wv0C16jwmYknMbu64J0xj8oreuf1QNo"
+api_key = ""
+bot_token = ""
 bot = telebot.TeleBot(bot_token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
